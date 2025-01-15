f9f48050b01bde36696dad28628a041e6f1c79fc
santosg572 <santosg572@gmail.com>
2025-01-15 21:13:49 +0000

    Test comité

---
+++ docs/source/traducir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+trans -brief :es 'Translate Shell also offers an interactive mode, the following example shows how to launch the interactive mode to translate content from Spanish to English'
+
+
